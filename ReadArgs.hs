04b8105edfd18655c30f117131746102fb849b7c
Noah Luck Easterly <noah.easterly@gmail.com>
2011-12-23 17:36:45 -0500

    got optional and multiple arguments working

--- ReadArgs.hs
+++ ReadArgs.hs
@@ -1,47 +1,116 @@
-{-# LANGUAGE ScopedTypeVariables, DeriveDataTypeable #-}
+{-# LANGUAGE TypeSynonymInstances, TypeOperators #-}
 module ReadArgs where
 
-import Control.Monad.Identity
-import Data.Typeable
+import Data.Maybe 
+import Data.List 
+
 import System.Environment
 import System.Exit
 import System.IO
 
-newtype UnquotedString = Unquoted String deriving (Show, Eq, Typeable)
+-- a class for types that can be parsed from exactly one command line argument
+class Arguable a where
+  parse :: String -> Maybe a
+  name :: a -> String
 
-instance Read UnquotedString where
-  readsPrec _ s = [(Unquoted s,"")]
+-- XXX: Ideally, we'd just let any type that is has Read and Typeable
+--      instances be types of Arguable, but this would compel all strings to
+--      be double quoted.
+--      Using UndecidableInstances to resolve this doesn't seem to work either
+instance Arguable Bool where
+  parse s = case reads s of
+    [(i,"")] -> Just i
+    otherwise -> Nothing
+  name _ = "Bool"
 
-class ArgTuple at where 
-  types :: at -> [TypeRep]
-  useArgs :: ArgTuple at' => at' -> [String] -> IO at
-  readArgs :: ArgTuple at => IO at
+instance Arguable Int where
+  parse s = case reads s of
+    [(i,"")] -> Just i
+    otherwise -> Nothing
+  name _ = "Int"
 
+instance Arguable String where
+  parse = Just
+  name _ = "String"
 
-usage :: ArgTuple at => at -> IO a
-usage at = do
-  progName <- getProgName
-  hPutStrLn stderr $ foldl (\u tr -> u ++ " <" ++ showsTypeRep tr ">") ("usage: " ++ progName) (types at)
-  exitFailure
+-- a class for types that can be parsed from some number of command line
+-- arguments
+class Argument a where
+  parseArg :: [String] -> [(a, [String])]
+  argName :: a -> String
 
-instance (Typeable a, Read a) => ArgTuple (Identity a) where
-  types _ = [typeOf (undefined :: a)]
-  useArgs _ [s] | [(a,"")] <- reads s = return (Identity a)
-  useArgs at _ = usage at
-  readArgs = getArgs >>= useArgs (undefined :: Identity a)
+-- use Require when it should be parsed from exactly one
+data Require a = Require { unRequire :: a }
+instance Arguable a => Argument (Require a) where
+  parseArg [] = []
+  parseArg (s:ss) = do
+    a <- maybeToList $ parse s
+    return (Require a, ss)
+  argName = name . unRequire
 
-instance (Typeable a, Read a, Typeable b, Read b) => ArgTuple (a,b) where
-  types _ = typeOf (undefined :: a) : types (undefined :: Identity b)
-  useArgs at (s:ss) | [(a,"")] <- reads s = do
-    Identity b <- useArgs at ss
-    return (a, b)
-  useArgs at _ = usage at
-  readArgs = getArgs >>= useArgs (undefined :: (a,b))
+-- use Maybe when it should be parsed from zero or one
+instance Arguable a => Argument (Maybe a) where
+  argName ~(Just x) = "["++name x++"]"
+  parseArg [] = [(Nothing, [])]
+  parseArg ss'@(s:ss) = case parse s of
+    Nothing -> [(Nothing, ss')]
+    justA   -> [(justA, ss)]
 
-instance (Typeable a, Read a, Typeable b, Read b, Typeable c, Read c) => ArgTuple (a,b,c) where
-  types _ = typeOf (undefined :: a) : types (undefined :: (b,c))
-  useArgs at (s:ss) | [(a,"")] <- reads s = do
-    (b,c) <- useArgs at ss
-    return (a, b, c)
-  useArgs at _ = usage at
-  readArgs = getArgs >>= useArgs (undefined :: (a,b,c))
+-- use a list when it should be parsed from zero or more
+instance Arguable a => Argument [a] where
+  argName ~(x:_) = "["++name x ++"...]"
+  parseArg ss = inits ss' `zip` tails ss
+    where ss' = map fromJust . takeWhile isJust $ map parse ss
+
+-- a class for tuples of types that can be parsed from the entire list
+-- of arguments
+class ArgumentTuple a where
+  readArgsFrom :: [String] -> [a]
+  usage :: a -> String
+
+-- use () for no arguments
+instance ArgumentTuple () where
+  readArgsFrom [] = [()]
+  readArgsFrom _ = []
+  usage = const ""
+
+-- use :& to construct arbitrary length tuples of any parsable arguments
+data a :& b = a :& b
+infixr 5 :&
+instance (Argument a, ArgumentTuple y) => ArgumentTuple (a :& y) where
+  readArgsFrom ss = do
+    (a, ss') <- parseArg ss
+    y <- readArgsFrom ss'
+    return $ a :& y
+  usage ~(a :& y) = " " ++ argName a  ++ usage y
+
+-- Use :& to derive instances for all the normal tuple types
+instance (Argument b, Argument a) => ArgumentTuple (b,a) where
+  readArgsFrom ss = do
+    b :& a :& () <- readArgsFrom ss
+    return (b,a)
+  usage ~(b,a) = usage (b :& a :& ())
+
+instance (Argument c, Argument b, Argument a) => ArgumentTuple (c,b,a) where
+  readArgsFrom ss = do
+    c :& b :& a :& () <- readArgsFrom ss
+    return (c,b,a)
+  usage ~(c,b,a) = usage (c :& b :& a :& ())
+
+instance (Argument d, Argument c, Argument b, Argument a) => ArgumentTuple (d,c,b,a) where
+  readArgsFrom ss = do
+    d :& c :& b :& a :& () <- readArgsFrom ss
+    return (d,c,b,a)
+  usage ~(d,c,b,a) = usage (d :& c :& b :& a :& ())
+
+-- parse the desired argument tuple from the command line or print
+-- a simple usage statment
+readArgs :: ArgumentTuple a => IO a
+readArgs = do
+  as@(~(a:_)) <- readArgsFrom `fmap` getArgs
+  case as of 
+    [] -> do 
+      progName <- getProgName
+      hPutStrLn stderr $ "usage: " ++ progName ++ usage a
+      exitFailure
+    _ -> return a
--- ReadArgsEx.hs
+++ ReadArgsEx.hs
@@ -1,10 +1,11 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 
-import Control.Monad.Identity
 import Data.List
 import ReadArgs
 
 main = do 
-  (n :: Int, Unquoted b, Unquoted s) <- readArgs
-  putStrLn . intercalate b $ replicate n s
+  ( Require n :: Require Int, 
+    b :: Maybe String, 
+    xs :: [String]) <- readArgs
+  print (n,b,xs)
